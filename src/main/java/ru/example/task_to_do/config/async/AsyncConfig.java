a9643ec925473b204d668cb71285187b64b624ed
aim_41tt <0505mustik@gmail.com>
2024-10-08 21:24:10 +0300

    enable async

--- src/main/java/ru/example/task_to_do/config/async/AsyncConfig.java
+++ src/main/java/ru/example/task_to_do/config/async/AsyncConfig.java
@@ -3,11 +3,13 @@ package ru.example.task_to_do.config.async;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
 
     @Bean(name = "taskExecutor")
